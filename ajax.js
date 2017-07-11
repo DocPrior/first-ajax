2610225cd29a280f40799f5c018cc75843f300e3
DocPrior <Sara Prior>
2017-07-11 16:03:47 -0400

    finish step 2

--- ajax.js
+++ ajax.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
+  var button12 = document.querySelector('#button12');
+  button12.addEventListener('click', function() {
+    $.ajax({
+      url: ____,
+      method: ___,
+      data: _____,
+      dataType: _____,
+    });
+  });
 
-  /* Your code goes here */
+
 
 });
